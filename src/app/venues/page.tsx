ad6aa0e13ef31388c490710b238e425bde412289
Shreya Gupta <134310104+Shreya020404@users.noreply.github.com>
2024-10-04 08:01:42 +0530

    Update page.tsx

--- src/app/venues/page.tsx
+++ src/app/venues/page.tsx
@@ -19,7 +19,7 @@ export default function Venues() {
     const fetchVenues = async () => {
       try {
         const response = await axios.get<Venue[]>(
-          "http://127.0.0.1:8080/api/venues"
+          "http://localhost:8080/api/venues"
         );
         setVenues(response.data);
       } catch (error) {
